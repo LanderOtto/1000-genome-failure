6f3789162138be81bd9d0c2b635b9fae2d5cf656
LanderOtto <alberto.mulone@unito.it>
2025-05-01 16:36:35 -0400

    fix

--- plotting/timeline.py
+++ plotting/timeline.py
@@ -33,7 +33,7 @@ def main(args):
 
     # Status
     pattern_status = re.compile(
-        r"^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}) DEBUG +Job ([^\s]+) changed status to ([A-Z]+)$"
+        r"^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3})\sDEBUG\s+Job ([^\s]+) changed status to ([A-Z]+)$"
     )
     # Handling error
     pattern_handling = re.compile(
@@ -50,11 +50,21 @@ def main(args):
     transfer_error_2_pattern = re.compile(
         r"^Error creating file ([^\s]+) with path ([^\s]+) in locations \[(.*?)\]\.$"
     )
+    transfer_error_3_pattern = re.compile(r"^FAILED copy from (.+) to (.+)$")
+    transfer_error_4_pattern = re.compile(r"^Job ([^\s]+) has no locations$")
     scheduling_pattern = re.compile("a")
     output_process_1_pattern = re.compile(
         r"^Expected (\S+) token of type (\S+), got (\S+)\.$"
     )
     output_process_2_pattern = re.compile(r"Token (\S+) is not optional")
+
+    allocation_pattern = re.compile(
+        r"^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3})\s+DEBUG\s+Job\s+([^\s]+)\s+allocated\s+(.+)$"
+    )
+    local_alloc_pattern = re.compile(r"locally$")
+    remote_alloc_pattern = re.compile(r"on location\s+(.+)$")
+
+    storage_size_err_pattern =  re.compile(r"Storage (.+) with (.+) paths cannot have negative size: (.+)$")
 
     jobs = {}
     failures = {}
@@ -84,6 +94,23 @@ def main(args):
                 failures.setdefault(job_name, []).append(
                     {"time": timestamp, "type": failure_type}
                 )
+            elif match_ := allocation_pattern.match(line):
+                timestamp, job_name, allocation = match_.groups()
+                if match_ := local_alloc_pattern.match(allocation):
+                    location_name = match_.group()
+                elif match_ := remote_alloc_pattern.match(allocation):
+                    location_name = match_.groups()[0]
+                timestamp = (
+                    datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f")
+                    - workflow_start
+                )
+                jobs.setdefault(job_name, []).append(
+                    {
+                        "time": timestamp,
+                        "status": "ALLOCATED",
+                        "location": location_name,
+                    }
+                )
             elif match_ := error_line_pattern.match(line):
                 timestamp, error_message = match_.groups()
                 timestamp = (
@@ -92,9 +119,12 @@ def main(args):
                 )
                 if failed_job_pattern.match(error_message):
                     errors.append({"time": timestamp, "message": "executing"})
-                elif transfer_error_1_pattern.match(
-                    error_message
-                ) or transfer_error_2_pattern.match(error_message):
+                elif (
+                    transfer_error_1_pattern.match(error_message)
+                    or transfer_error_2_pattern.match(error_message)
+                    or transfer_error_3_pattern.match(error_message)
+                    or transfer_error_4_pattern.match(error_message)
+                ):
                     errors.append({"time": timestamp, "message": "transferring"})
                 elif output_process_1_pattern.match(
                     error_message
@@ -102,6 +132,8 @@ def main(args):
                     errors.append({"time": timestamp, "message": "retrieving"})
                 elif scheduling_pattern.match(error_message):
                     errors.append({"time": timestamp, "message": "initializing"})
+                elif storage_size_err_pattern.match(error_message):
+                    print("Warning: storage size error")
                 else:
                     raise Exception(f"Unexpected error: {error_message}")
     # print(serialize_jobs(jobs))
@@ -110,6 +142,7 @@ def main(args):
     combined_results = {}
     for job_name, job_list in jobs.items():
         for job in job_list:
+            kargs = {}
             if job["status"] == "RECOVERY":
                 err = {
                     "time": None,
@@ -130,12 +163,13 @@ def main(args):
                             )
                         err["time"] = nearest_err[0]
                         err["error_type"] = nearest_err[1]["message"]
-
+            elif job["status"] == "ALLOCATED":
+                kargs |= {"location": job["location"]}
             combined_results.setdefault(job_name, []).append(
-                {"time": job["time"], "status": job["status"]}
+                {"time": job["time"], "status": job["status"]} | kargs
             )
 
-    print(json.dumps(serialize_jobs(combined_results), indent=2))
+    # print(json.dumps(serialize_jobs(combined_results), indent=2))
     print(
         "Number of steps",
         len(
